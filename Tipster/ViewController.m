819d066e6a54fee0bfe0a0d8f78dd0f698bccb6f
Githel Lynn Suico <githelsuico@fb.com>
2020-06-23 15:07:10 -0700

    storyboard updates

--- Tipster/ViewController.m
+++ Tipster/ViewController.m
@@ -10,6 +10,10 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *billField;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+
 @end
 
 @implementation ViewController
@@ -19,5 +23,18 @@
     // Do any additional setup after loading the view.
 }
 
+- (IBAction)onTap:(id)sender {
+    NSLog(@"Hello");
+    [self.view endEditing:YES];
+}
+
+- (IBAction)onEdit:(id)sender {
+    double bill = [self.billField.text doubleValue];
+    double tip = 0.2 * bill;
+    double total = bill + tip;
+    
+    self.tipLabel.text = [NSString stringWithFormat:@"$%f", tip];
+    self.totalLabel.text = [NSString stringWithFormat:@"$%f", total];;
+}
 
 @end
