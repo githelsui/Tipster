21041ae8dc688319ce6c57f5cd2f093121f371fe
Githel Lynn Suico <githelsuico@fb.com>
2020-06-23 15:24:36 -0700

    attach segmented control

--- Tipster/ViewController.m
+++ Tipster/ViewController.m
@@ -13,6 +13,7 @@
 @property (weak, nonatomic) IBOutlet UITextField *billField;
 @property (weak, nonatomic) IBOutlet UILabel *tipLabel;
 @property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
 
 @end
 
@@ -30,11 +31,15 @@
 
 - (IBAction)onEdit:(id)sender {
     double bill = [self.billField.text doubleValue];
-    double tip = 0.2 * bill;
+    
+    NSArray *percentages = @[@(0.15), @(0.20), @(0.22)];
+    double tipPercentage = [percentages[self.tipControl.selectedSegmentIndex] doubleValue];
+    
+    double tip = tipPercentage * bill;
     double total = bill + tip;
     
-    self.tipLabel.text = [NSString stringWithFormat:@"$%f", tip];
-    self.totalLabel.text = [NSString stringWithFormat:@"$%f", total];;
+    self.tipLabel.text = [NSString stringWithFormat:@"$%.2f", tip];
+    self.totalLabel.text = [NSString stringWithFormat:@"$%.2f", total];
 }
 
 @end
